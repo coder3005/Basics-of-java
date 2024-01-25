6ed9b0626783d41f5b4fb6de9822a0dd0d2ef416
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:36:47 +0530

    Add files via upload

---
+++ javabasics.java
@@ -0,0 +1,9 @@
+package Basics;
+public class javabasics {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");  // line ka space
+        System.out.print("Hello World!\n World!\n");  // line ka space
+        System.out.println("Hello World!");  // line ka space
+    }
+}
+//boilerplate code
