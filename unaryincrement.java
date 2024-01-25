f3308471faee29ea4808d4b67cfcab02c99489c2
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:40:31 +0530

    Create unaryincrement.java

---
+++ unaryincrement.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class unaryincrement {
+    public static void main(String arsgs[]) {
+        int a=10;
+        int b=++a; // pre increment
+        int c=a++; // post increment
+        int d=15;
+        int e=d++;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+    }
+}
