929b9d8923ae9f493ba24aac3553dcb9383502bd
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:39:05 +0530

    Create javabasics3.java

---
+++ javabasics3.java
@@ -0,0 +1,17 @@
+package Basics;
+public class javabasics3 {
+    public static void main(String args[])  {
+        byte b=8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        //boolean var = false;
+        //float price = 10.5f;
+        //int number = 5;
+        //long
+        //double
+        //short n=240;
+        
+
+    }
+}
