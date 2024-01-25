8c9619560349856a8948e227148296aa73ce2f39
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:38:06 +0530

    Create javabasics1.java

---
+++ javabasics1.java
@@ -0,0 +1,9 @@
+package Basics;
+public class javabasics1 {
+    public static void main(String args[]) {
+        System.out.println("****");  // line ka space
+        System.out.println("***");  // line ka space
+        System.out.println("**");  // line ka space
+        System.out.println("*");  // line ka space
+    }
+}
