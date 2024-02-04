df1f6841feb428e89e9d0f95d3536ca63a4c19e4
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 21:48:21 +0530

    Create typerconversion.java

---
+++ Type Conversion/typerconversion.java
@@ -0,0 +1,14 @@
+package TypeConversion;
+import java.util.*;
+
+public class typeconversion {
+    public static void main(String args[]) {
+        Scanner  sc=new Scanner(System.in);
+        float number=sc.nextInt();
+        System.out.println(number);
+        sc.close();
+/* if we write 
+int number=sc.nextFloat(); it will give error
+ */
+    }
+}
