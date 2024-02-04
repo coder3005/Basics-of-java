c5ec9ee175802adc0a3b0d144a511aef7ad77128
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 21:45:48 +0530

    Create typepromotion1.java

---
+++ Type Promotion/typepromotion1.java
@@ -0,0 +1,10 @@
+package TypePromotion;
+public class typepromotion1 {
+    public static void main(String[] args) {
+        short a=5;
+        byte b=25;
+        char c='c';
+        byte bt=(byte)(a+b+c);
+        System.out.println(bt);
+    }    
+}
