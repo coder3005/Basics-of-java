ad493bfee7165c82e58f52ae274cdbcfc2f2990a
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 10:06:14 +0530

    Create javabasics4.java

---
+++ javabasics4.java
@@ -0,0 +1,16 @@
+package Basics;
+import java.util.*;
+
+public class javabasics {
+    public static void main(String[] args) {
+        boolean[] bool=new boolean[5];
+        int[] num=new int[10];
+        char[] ch=new char[10];
+        float[] fl=new float[10];
+
+        System.out.println("Bool : "+ Arrays.toString(bool));
+        System.out.println("Num : "+ Arrays.toString(num));
+        System.out.println("Char : "+ Arrays.toString(ch));
+        System.out.println("Float : "+ Arrays.toString(fl));
+    }
+}
