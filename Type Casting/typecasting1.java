3261c1923acceb36cdf2804e7f98ac79f7c17b91
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 21:50:10 +0530

    Create typecasting1.java

---
+++ Type Casting/typecasting1.java
@@ -0,0 +1,16 @@
+package TypeCasting;
+
+import java.util.*;
+
+public class typecasting1 {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        char ch='a';
+        char ch2='b';
+        int number=ch;
+        int number2=ch2; 
+        System.out.println(number);
+        System.out.println(number2);
+        sc.close();
+    }
+}
